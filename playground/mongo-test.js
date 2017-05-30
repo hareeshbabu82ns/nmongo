340b3c510a88f43a57d98c0f0a009af2be86da62
Hareesh <hareeshbabu82ns@gmail.com>
2017-05-29 20:36:07 -0600

    initial commit

---
+++ playground/mongo-test.js
@@ -0,0 +1,49 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/Todos', (err, db) => {
+  if (err) {
+    return console.log('Connection to MongoDB failed');
+  }
+  console.log('Connected to MongoDB');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'todo nodejs',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log(`Undable to Crete TODO`, err);
+  //   }
+  //   console.log(`Todo Created`, JSON.stringify(result.ops, undefined, 2));
+  // })
+
+  // db.collection('Users').insertOne({
+  //   name: 'Hareesh Babu Polla',
+  //   age: 33,
+  //   location: 'Canada'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Failed to create User document', err);
+  //   }
+  //   console.log(`User Document Created at ${result.ops._id.getTimeStamp()}`,
+  //     JSON.stringify(result.ops, undefined, 2));
+  // });
+
+
+  //Find All
+  db.collection('Todos').find().toArray().then((todos) => {
+    console.log('Todos:', JSON.stringify(todos, undefined, 2));
+  });
+
+  //Find by property
+  db.collection('Todos').find({ completed: true }).toArray().then((todos) => {
+    console.log('Completed Todos:', JSON.stringify(todos, undefined, 2));
+  });
+
+  //Find by ID (using ObjectID)
+  db.collection('Todos').find({ _id: new ObjectID('592c125844210b37d026c1e0') })
+    .toArray().then((todos) => {
+      console.log('Todos by ID:', JSON.stringify(todos, undefined, 2));
+    });
+
+  // db.close();
+})
